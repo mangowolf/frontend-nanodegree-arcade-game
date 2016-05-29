fc785cb9467e4b76d6a30433cf5fa9446dabc9bc
mangowolf <jimmyz949@hotmail.com>
2016-05-29 10:22:30 -0700

    Initial Commit Gameboard Rendered, Enemy and Player classes defined

--- js/app.js
+++ js/app.js
@@ -24,12 +24,26 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var player = function(){
+
+	//load character sprite using helper
+	this.sprite = 'images/char-boy.png';
+}
+
+player.prototype.update = function(dt){
+
+};
+
+player.prototype.render = function(){
+	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var allEnemies = [new Enemy()];
+var player = new player();
 
 
 // This listens for key presses and sends the keys to your
